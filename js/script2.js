b3bba917120dbc813c991cb06dbe4c340c94ebb1
Michał Kowalczyk <michalkowal3k@gmail.com>
2023-11-10 23:12:19 +0100

    New repository created

---
+++ js/script2.js
@@ -0,0 +1,16 @@
+{
+    const calculateValue = (event) => {
+        event.preventDefault();
+
+        const amount = document.querySelector(".js-A");
+        const exchangeRate = document.querySelector(".js-eR");
+        const value = amount.value * exchangeRate.value;
+        const finalValue = document.querySelector(".js-finalValue");
+        finalValue.innerText = value;
+    };
+
+    {
+        const money = document.querySelector(".js-currency")
+        money.addEventListener("submit", calculateValue)
+    }
+}
